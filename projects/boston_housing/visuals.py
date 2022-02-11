604d25fb7f7de2827cfef1fc9fc2e2a09a887d8d
Aatechexe <mochammadasadurofiq@gmail.com>
2022-02-12 00:57:09 +0700

    add the

--- projects/boston_housing/visuals.py
+++ projects/boston_housing/visuals.py
@@ -17,7 +17,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import ShuffleSplit, train_test_split
 
 def ModelLearning(X, y):
-    """ Calculates the performance of several models with varying sizes of training data.
+    """ Calculates the the performance of several models with varying sizes of training data.
         The learning and testing scores for each model are then plotted. """
 
     # Create 10 cross-validation sets for training and testing
